4ccb7935f0d61a655a106904cd5dafe6959be54b
Daniel Bagley <dseph2000@hotmail.com>
2022-10-14 14:37:24 -0400

    updated version updated notes

--- EasInspector/Properties/AssemblyInfo.cs
+++ EasInspector/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("EASInspector")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Fiddler inspector to convert EAS WBXML into XML")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("EASInspector")]
-[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyCopyright("Copyright © 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.0.0")]
-[assembly: AssemblyFileVersion("1.6.0.0")]
+[assembly: AssemblyVersion("1.5.0.0")]
+[assembly: AssemblyFileVersion("1.5.0.0")]
 
 [assembly: Fiddler.RequiredVersion("4.6.0.0")]
